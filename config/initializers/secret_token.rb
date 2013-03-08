c26e963fd82b857c20afe65f65b627b2b7a65fac
James Watt <james@wa.tt>
2013-03-07 17:56:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watt::Application.config.secret_token = 'f131792c18ed233e66cf74e349185799c325560cfbd760f35c74a239709416a97152139b357ab51eed422ab5207da6a032e0ca2bfb18a7b4d5872bec0bfd2584'
